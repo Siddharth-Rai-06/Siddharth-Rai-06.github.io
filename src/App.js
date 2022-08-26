d3e0f985a23f91268e01e13679bcf54e0adfb247
Siddharth Rai <sidrai0620@gmail.com>
2022-08-27 00:54:48 +0530

    Updated Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Footer from "./components/footer";
 import Header from "./components/header";
-
 function App() {
   return (
     <>
     <Header/>
-    <Footer/>
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,23 @@
-const Header=()=>{
+const Header = () => {
     return (
-        <h1>Hello to the header</h1>
+        <>
+            <div className="header">
+                <div className="data-1">Siddharth Rai</div>
+                <div className="data-2">
+                    <div className="header-cat">category - 1</div>
+                    <div className="header-cat">category - 2</div>
+                    <div className="header-cat">category - 3</div>
+                </div>
+                <div className="data-3">
+                    <div className="header-con-1">
+                        +91-7080139778
+                    </div>
+                    <div className="header-con-2">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
