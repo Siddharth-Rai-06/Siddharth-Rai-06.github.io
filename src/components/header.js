012a2742d0071be0187a60b61cdd4d3e3cf5feab
Siddharth Rai <sidrai0620@gmail.com>
2022-08-27 18:18:20 +0530

    Header Update 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,28 @@
 import { useEffect } from "react";
 const Header = () => {
 
-     // Sticky Menu Area
-     useEffect(() => {
+    useEffect(() => {
         window.addEventListener('scroll', isSticky);
         return () => {
             window.removeEventListener('scroll', isSticky);
         };
     });
 
-           
-    /* Method that will fix header after a specific scrollable */
-           const isSticky = (e) => {
-                const header = document.querySelector('.header');
-                const scrollTop = window.scrollY;
-                scrollTop >= 250 ? header.classList.add('is-sticky') : header.classList.remove('is-sticky');
-            };
 
-    // function init() {
-    //     window.addEventListener('scroll', function(e){
-    //         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-    //             shrinkOn = 300,
-    //             header = document.querySelector(".header-top");
-    //         if (distanceY > shrinkOn) {
-    //             classie.add(header,"smaller");
-    //         } else {
-    //             if (classie.has(header,"smaller")) {
-    //                 classie.remove(header,"smaller");
-    //             }
-    //         }
-    //     });
-    // }
-    // window.onload = init();
+    const isSticky = (e) => {
+        const header = document.querySelector('.header');
+        const scrollTop = window.scrollY;
+        scrollTop >= 250 ? header.classList.add('is-sticky') : header.classList.remove('is-sticky');
+    };
 
     return (
         <>
             <div className="header" id="myHeader">
                 <div className="data-1">Siddharth Rai</div>
                 <div className="data-2">
-                    <div className="header-cat">category - 1</div>
-                    <div className="header-cat">category - 2</div>
-                    <div className="header-cat">category - 3</div>
+                    <div className="header-cat" style={{borderLeft:"hidden"}}>About Me</div>
+                    <div className="header-cat">Education</div>
+                    <div className="header-cat">Projects</div>
                 </div>
                 <div className="data-3">
                     <div className="header-con-1">
